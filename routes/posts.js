b6f154598ebc833c5020b95f939fbd7c0837b6fd
ojogoddy <godwin5867@gmail.com>
2025-09-03 06:56:38 +0100

    third

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
+const Post = require('../models/posts');
 const authMiddleware = require('../middleware/authMiddleware'); 
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
